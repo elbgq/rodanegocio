a4e48948a9020dcdc89f2786edffa6441faf8e04
eloi lerner <eloibgq@gmail.com>
2026-04-09 16:30:00 -0300

    Ajuste no settings e template mesa_relatorio

--- rodanegocios/settings.py
+++ rodanegocios/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!nq0pj3jwa=eq0lv7dh5_s@%s94sj52*c9gak_%cyzi42mpzyc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rodanegocio-eloibgq.pythonanywhere.com']
+ALLOWED_HOSTS = ['rodanegocio-eloibgq.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
