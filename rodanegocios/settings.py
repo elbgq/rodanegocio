8f292b5c4d4f3357b7364b1a33a3db6aa77efbc3
eloi lerner <eloibgq@gmail.com>
2026-04-16 23:18:56 -0300

    Alteração do settings

--- rodanegocios/settings.py
+++ rodanegocios/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.RodanegociosProtectionMiddleware',
+    #'core.middleware.RodanegociosProtectionMiddleware',
     
 ]
 
